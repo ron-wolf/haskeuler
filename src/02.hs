08e0c7bcadf3a40510a55872d905f3e55e9d07f9
Ron Wolf <ronatwork246@gmail.com>
2017-10-25 06:18:56 -0400

    Simplify
    Replace custom `stop` function with built-in `takeWhile` w/reversed condition

--- src/02.hs
+++ src/02.hs
@@ -1,24 +1,12 @@
 module Main where
 
--- utility functions
-
-stop :: (a -> Bool) -> [a] -> [a]
-stop = stop' []
-
-stop' :: [a] -> (a -> Bool) -> [a] -> [a]
-stop' acc test [] = acc
-stop' acc test (x:xs) =
-   if   test x
-   then acc
-   else stop' (acc ++ [x]) test xs
-
 fibs :: [Word]
 fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
 
 -- work
 
 result :: Word
-result = sum $ filter even $ stop (> 4000000) fibs
+result = sum $ filter even $ takeWhile (< 4000000) fibs
 
 main :: IO ()
 main = print result
